390c905f20b6ea2ea77c556f831b03850d231cad
Kratious <semjinghan@gmail.com>
2021-01-25 23:31:57 +0800

    Add rna-transcription solution

---
+++ rna-transcription/RnaTranscription.fs
@@ -0,0 +1,13 @@
+﻿module RnaTranscription
+
+let toRna (dna: string): string =
+    dna
+    |> Seq.map
+        (fun nucl ->
+            match nucl with
+            | 'G' -> 'C'
+            | 'C' -> 'G'
+            | 'T' -> 'A'
+            | 'A' -> 'U'
+            | _ -> failwith "invalid")
+    |> System.String.Concat
