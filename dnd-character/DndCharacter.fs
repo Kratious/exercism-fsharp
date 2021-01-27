3a05e9103aa25740ecb80be45c749ba8f4f8403c
Kratious <semjinghan@gmail.com>
2021-01-27 23:57:30 +0800

    Add dnd-character solution

---
+++ dnd-character/DndCharacter.fs
@@ -0,0 +1,34 @@
+﻿module DndCharacter
+
+open System
+
+type Character =
+    { Strength: int
+      Dexterity: int
+      Constitution: int
+      Intelligence: int
+      Wisdom: int
+      Charisma: int
+      Hitpoints: int }
+
+let modifier (x: int): int = (float (x - 10)) / 2.0 |> floor |> int
+
+let ability (): int =
+    let random = Random()
+
+    List.init 4 (fun _ -> random.Next(1, 7))
+    |> List.sort
+    |> List.tail
+    |> List.sum
+
+
+let createCharacter (): Character =
+    let constitution = ability ()
+
+    { Strength = ability ()
+      Dexterity = ability ()
+      Constitution = constitution
+      Intelligence = ability ()
+      Wisdom = ability ()
+      Charisma = ability ()
+      Hitpoints = 10 + modifier constitution }
